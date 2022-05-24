82c25965184bc6081da20aab7e179e5dceb9995c
Sebas Delgado <naitsabesdv@gmail.com>
2022-05-24 23:54:28 +0200

    Navmesh click to move around

---
+++ TFG_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
