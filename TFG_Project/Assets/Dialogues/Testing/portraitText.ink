943e02ff21818ffec44c910fdee5adb2cb271e7b
Sebas Delgado <naitsabesdv@gmail.com>
2022-06-28 01:48:58 +0200

    tag handleling correctly Ink files
    Now we can see portraits into the scene

---
+++ TFG_Project/Assets/Dialogues/Testing/portraitText.ink
@@ -0,0 +1,13 @@
+Hello there! #speaker:Sophia #portrait:sophia #layout:left
+-> main
+
+==main==
+Are you feeling better today, you have a better looking.
++[Yes, I am better]
+    Oh Cool, so you can help me to make dinner for today! 
+    - Actually, Tom and Mandy just called me to do some homework together since I missed my class.#speaker:Ava #portrait:ava #layout:left
+    
++[Well actually mom, I want ask you something...]
+    I'm all ears honey, speak up. #speaker:Sophia #portrait:sophia #layout:right
+    
+-> END
--- TFG_Project/Assets/Scripts/Dialogue/DialogueManager.cs
+++ TFG_Project/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -13,6 +13,11 @@ public class DialogueManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI dialogueText;
 
+    [SerializeField] private TextMeshProUGUI displayNameText;
+
+    [SerializeField] private Animator portraitAnimator;
+    private Animator layoutAnimator;
+
     [Header("Choices UI")]
     [SerializeField] private GameObject[] choices;
     private TextMeshProUGUI[] choicesText;
@@ -22,6 +27,13 @@ public class DialogueManager : MonoBehaviour
     private Story currentStory;
 
     public bool dialogueIsPlaying { get; private set;}
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+    private const string LAYOUT_TAG = "layout";
+    private const string POINTS_AVASOPHIA_TAG = "as_points";
+    private const string POINTS_AVAPETER_TAG = "ap_points";
+
 
     private void Awake()
     {
@@ -42,6 +54,9 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
+
+        //get the layout animator
+        layoutAnimator = dialoguePanel.GetComponent<Animator>();
 
         choicesText = new TextMeshProUGUI[choices.Length];
         int index = 0;
@@ -73,6 +88,12 @@ public class DialogueManager : MonoBehaviour
         dialogueIsPlaying = true;
         dialoguePanel.SetActive(true);
 
+        //restart portrait, layouot, and speaker
+        displayNameText.text = "???";
+        portraitAnimator.Play("dedfault");
+        layoutAnimator.Play("right");
+
+
         ContinueStory();
     }
 
@@ -92,10 +113,53 @@ public class DialogueManager : MonoBehaviour
 
             //display choices, if any, for this dialogue line
             DisplayChoices();
+
+            //handle tags
+
+            HandleTags(currentStory.currentTags);
         }
         else
         {
             ExitDialogueMode();
+        }
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        //loop through each tag and handle it accordingly
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(':');
+            if(splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);
+            }
+
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            //Switch to handle each specific tag
+            switch(tagKey)
+            {
+                case SPEAKER_TAG:
+                    displayNameText.text = tagValue;
+                    break;
+                case LAYOUT_TAG:
+                    layoutAnimator.Play(tagValue);
+                    break;
+                case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    break;
+                case POINTS_AVASOPHIA_TAG:
+                    Debug.Log("aspoints =" + tagValue);
+                    break;
+                case POINTS_AVAPETER_TAG:
+                    Debug.Log("appoints =" + tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but ius not currently being handled: " + tag);
+                    break;
+            }
         }
     }
 
